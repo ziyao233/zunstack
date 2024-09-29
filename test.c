06fd7d5f87011c63c63e9e7616d4fe1fdc5645e0
Yao Zi <ziyao@disroot.org>
2024-09-29 18:52:35 +0000

    Do vanilla fp-based unwinding

--- test.c
+++ test.c
@@ -2,12 +2,19 @@
 #include <string.h>
 
 int
+foo(char *a)
+{
+	a[1000000000] = 0;
+	return 0;
+}
+
+int
 main(void)
 {
 	int zunstack_init(void);
 	zunstack_init();
 
 	char a[20];
-	memcpy(a, NULL, 16);
+	foo(a);
 	return 0;
 }
--- zunstack.c
+++ zunstack.c
@@ -5,19 +5,59 @@
  *	Copyright (c) 2024 Yao Zi <ziyao@disroot.org>. All rights reserved.
  */
 
+#define _GNU_SOURCE
+
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <unistd.h>
 #include <signal.h>
 #include <ucontext.h>
 
-static
-void sighandler(int signo, siginfo_t *info, void *ctx)
-{
-	(void)signo; (void)info; (void)ctx;
-	puts("Catch you, SIGSEGV!");
 
+static int pipeFds[2];
+typedef unsigned long int Word;
+
+/*
+ *	On x86-64, %rbp points to the frame pointer
+ */
+static Word *
+next_frame(Word *frame, Word *pc)
+{
+	*pc = frame[1];
+	return (Word *)*frame;
+}
+
+static inline int
+is_valid_addr(Word *frame) {
+	return write(pipeFds[1], frame, 1) >= 0;
+}
+
+static void
+unwind_stack(ucontext_t *ctx)
+{
+	Word *frame = (Word *)ctx->uc_mcontext.gregs[REG_RBP];
+	Word pc = ctx->uc_mcontext.gregs[REG_RIP];
+
+	for (int i = 0;
+	     is_valid_addr(frame);
+	     frame = next_frame(frame, &pc)) {
+		printf("Frame #%d (0x%p): PC = 0x%lx\n", i, frame, pc);
+		i++;
+	}
+}
+
+static
+void sighandler(int signo, siginfo_t *info, void *pCtx)
+{
+	(void)signo; (void)info;
 	struct sigaction dfl = { .sa_handler = SIG_DFL };
+	sigaction(SIGSEGV, &dfl, NULL);
 	sigaction(SIGABRT, &dfl, NULL);
+
+	unwind_stack(pCtx);
+
 	abort();
 }
 
@@ -29,9 +69,11 @@ zunstack_init(void)
 					.sa_sigaction = sighandler
 			       };
 
-	int ret = sigaction(SIGSEGV, &act, NULL);
-	if (ret < 0)
-		return ret;
+	if (pipe(pipeFds) < 0)
+		return -1;
+
+	if (sigaction(SIGSEGV, &act, NULL))
+		return -1;
 
 	return sigaction(SIGABRT, &act, NULL);
 }
